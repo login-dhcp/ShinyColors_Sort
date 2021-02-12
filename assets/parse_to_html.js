f2706b97be270508b658c7353d5e2b762f0f715d
login_dhcp <nonamed424@gmail.com>
2021-02-12 19:44:21 +0900

    update dataset, variable names
    - change dataset structure
    - add more descriptions

--- assets/parse_to_html.js
+++ assets/parse_to_html.js
@@ -1,80 +1,36 @@
-const songToHTML = (data) => {
+function findItemByKeyValue(key, value) {
+    items = dataset['Items'];
+    for (i=0; i<items.length; i++) {
+        item = items[i];
+        if (item[key] === value) {
+            return item;
+        }
+    }
+}
+
+function findItemByName(value) {
+    return findItemByKeyValue('name', value);
+}
+
+const itemToHTML = (data) => {
     return `<li><label style="cursor:pointer">
-<input type="checkbox" data-checkbox-id="${data['series']}_${data['album']}_${data['song']}" class="select_one">
-<span>${data['name']}</span>
+<input type="checkbox" data-checkbox-id=">${data['name']}" class="select_one">
+<span title='${get_description(data)}'>${data['name']}</span>
 </label></li>`
 };
 
-const albumToHTML = (data) => {
-    html = `<li>
-<label style="cursor:pointer">
-    <input type="checkbox" data-checkbox-id="${data['series']}_${data['album']}" class="select"><span>${data['name']}</span>
-</label>
-<div class="collapsible"> | 펼치기</div>
-<ul class="collapsible_content">`
-
-    songs_list = dataset['Songs'];
-
-    var songs_cnt = 0;
-    for (let i = 0; i < songs_list.length; i++) { // TODO use better algorithm
-        song = songs_list[i];
-        if (song['album'] === data['album']) {
-            songs_cnt += 1;
-            html = html.concat(' ', songToHTML(song));
-        }
-    }
-    html = html.concat(' ', '</ul></li>');
-    if (songs_cnt === 0) {
-        return ''
-    } else { return html }
-
-};
-
-const seriesToHTML = (data) => {
-    series_common = 'THE IDOLM@STER SHINY COLORS';
-    if (data['name'].startsWith(series_common)) {
-        name = data['name'].replace(series_common, '');
-    } else {
-        name = data['name'];
-    }
-
-    // html = `<ul><span>${name} series</span><li>`
-    html = `<ul><li>`
-    html = html.concat('', `<label style="cursor:pointer">
-        <input type="checkbox" data-checkbox-id="${data['series']}" class="select"><span>${name} series</span>
-    </label><div class="collapsible"> | 펼치기</div>
-    <ul class="collapsible_content">`)
-
-
-    albums_list = dataset['Albums'];
-
-    var albums_cnt = 0;
-    for (let i = 0; i < albums_list.length; i++) { // TODO use better algorithm
-        album = albums_list[i];
-        if (album['series'] === data['series']) {
-            albums_cnt += 1;
-            html = html.concat(' ', albumToHTML(album));
-        }
-    }
-    html = html.concat(' ', '</ul></li></ul>');
-        if (albums_cnt === 0) {
-        return ''
-    } else { return html }
-}
-
-
 $(parse_dataset())
+
 function parse_dataset() {
-    // checkboxs = document.getElementById("sort_list_checkbox");
     checkboxs = document.getElementById("sort_list");
 
-    series = dataset['Series'];
-    for (let i = 0; i < series.length; i++) {
-        checkboxs.insertAdjacentHTML('beforeend', seriesToHTML(series[i]));
+    sort_items = dataset['Items'];
+
+    for (let i = 0; i < sort_items.length; i++) {
+        checkboxs.insertAdjacentHTML('beforeend', itemToHTML(sort_items[i]));
     }
 
     var coll = document.getElementsByClassName("collapsible");
-
     for (let i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
@@ -87,4 +43,14 @@ function parse_dataset() {
         });
     }
 
+}
+
+
+////////////////////
+function get_description(data) {
+    const clone = JSON.parse(JSON.stringify(data));
+    delete clone['image'];
+    delete clone['audio'];
+    var str = JSON.stringify(clone, null, 2);
+    return str
 }
---
+++ assets/utils.js
@@ -0,0 +1,135 @@
+////////////////////////
+// collapsible select buttons
+
+function selectAll() {
+    changeAllCheckboxTo(true);
+}
+
+function selectNone() {
+    changeAllCheckboxTo(false);
+}
+
+function changeAllCheckboxTo(value) {
+    var checkboxs = document.querySelectorAll(`input[data-checkbox-id]`);
+    for (let i = 0; i < checkboxs.length; i++) {
+        // $(inputElements[i]).prop("checked", value);
+        if (checkboxs[i].checked !== value) {
+            checkboxs[i].click();
+        }
+    }
+    return;
+}
+
+// code for parent checkboxs selecting all childs
+$(function() {
+    var checkboxs = document.querySelectorAll(`input[data-checkbox-id]`);
+    for (let i = 0; i < checkboxs.length; i++) {
+        var parent_checkbox_id = checkboxs[i].getAttribute('data-checkbox-id');
+        var child_checkboxs = document.querySelectorAll(`input[data-checkbox-id^="${parent_checkbox_id}_"]`);
+        // if isparent
+        if (child_checkboxs.length != 0) {
+            // link childs to parents
+            $(checkboxs[i]).change(function() {
+                var parent_checkbox_id = checkboxs[i].getAttribute('data-checkbox-id');
+                var child_checkboxs = document.querySelectorAll(`input[data-checkbox-id^="${parent_checkbox_id}_"]`);
+                for (let j = 0; j < child_checkboxs.length; j++) {
+                    $(child_checkboxs[j]).prop('checked', $(this).prop('checked'));
+                }
+            });
+
+            // 3 state checkboxs for parents
+            for (let j = 0; j < child_checkboxs.length; j++) {
+                child_checkboxs[j].addEventListener('change', function() {
+                    let cnt = 0;
+                    let my_checkbox_id = $(this).prop('dataset')['checkboxId'];
+                    let parent_checkbox_id = my_checkbox_id.substring(0, my_checkbox_id.length - 3);
+                    var parent_checkbox = document.querySelectorAll(`input[data-checkbox-id="${parent_checkbox_id}"]`);
+                    var sibling_checkboxs = document.querySelectorAll(`input[data-checkbox-id^="${parent_checkbox_id}_"]`);
+
+                    for (let k = 0; k < sibling_checkboxs.length; k++) {
+                        if (sibling_checkboxs[k].checked) {
+                            cnt += 1;
+                        }
+                    }
+
+                    if (cnt === sibling_checkboxs.length) {
+                        $(parent_checkbox).
+                        prop("indeterminate", false).
+                        prop('checked', true);
+                    } else if (cnt === 0) {
+                        $(parent_checkbox).
+                        prop('indeterminate', false).
+                        prop('checked', false);
+                    } else {
+                        $(parent_checkbox).prop('indeterminate', true);
+                    }
+                });
+            }
+        }
+    }
+});
+
+// link startSort() to sort_start button
+document.getElementById("sort_start").addEventListener("click", startSort, false);
+
+// link select all/disselect all to buttons
+document.getElementById("btn_select_all").addEventListener("click", selectAll, false);
+document.getElementById("btn_select_none").addEventListener("click", selectNone, false);
+
+
+function open_collapsed() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", open_collapsed);
+}
+
+
+/////////////////////////////////////////
+// screen related things
+function setVSScreen(value1, value2) {
+    leftItem = findItemByName(value1);
+    let leftImage = document.getElementById('sort_left_image');
+    leftImage.src = leftItem['image'];
+    leftImage.width = 256;
+    leftImage.height = 256;
+    document.getElementById("sort_left_button").innerText = value1;
+    document.getElementById("sort_left_audio").src = leftItem['audio'];
+    leftDescription = document.getElementById('sort_left_description');
+    leftDescription.innerText = get_description(leftItem);
+
+    rightItem = findItemByName(value2);
+    let rightImage = document.getElementById('sort_right_image');
+    rightImage.src = rightItem['image'];
+    rightImage.width = 256;
+    rightImage.height = 256;
+    document.getElementById("sort_right_button").innerText = value2;
+    document.getElementById("sort_right_audio").src = rightItem['audio'];
+    rightDescription = document.getElementById('sort_right_description');
+    rightDescription.innerText = get_description(rightItem);
+}
+
+function beatutify(results) {
+    let result_str = 'Result<br />';
+    for (let i = 0; i < results.length; i++) {
+        result = results[i];
+        name = result[1][0];
+        rank = result[1][2];
+
+        item = findItemByName(name);
+        result_str += ` ${String(rank)}위: ${item['name']}<br />`
+        result_str += `<img class="img" src="${item['image']}" style='width:256; height:256;'><br />`
+        result_str += `<audio controls src="${item['audio']}"></audio><br />`; // todo if 'audio' is in item.keys()
+    }
+    return result_str;
+
+}
